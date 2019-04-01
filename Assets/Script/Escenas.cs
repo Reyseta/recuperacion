301f7ae066fc37a2977bc8515715e3039ab01c68
Reyseta <luis.arg.gar@gmail.com>
2019-04-01 13:50:25 +0200

    Initial commit

---
+++ Assets/Script/Escenas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Escenas : MonoBehaviour
+{
+   public void cambiarescena (string nombredeescena)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
